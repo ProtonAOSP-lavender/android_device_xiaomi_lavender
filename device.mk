a5d59171b41a4c72caead200e84b83a0b7def301
Ratoriku <a1063021545@gmail.com>
2021-08-30 09:49:34 +0800

    lavender: Decommonize vibrator hal
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Signed-off-by: raiyanbinmohsin <raiyanbinmohsinshishir@gmail.com>

--- device.mk
+++ device.mk
@@ -73,6 +73,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl:64 \
+    android.hardware.vibrator@1.0-service
+
 # Wifi
 PRODUCT_PACKAGES += \
     LavenderWifiOverlay
