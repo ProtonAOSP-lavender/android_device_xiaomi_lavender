02fb417d8f5595bb658c544bfa4ddf798e84d12f
raiyanbinmohsin <raiyanbinmohsinshishir@gmail.com>
2021-11-03 00:42:52 +0600

    lavender: Let's build ProtonAOSP
    Signed-off-by: raiyanbinmohsin <raiyanbinmohsinshishir@gmail.com>

--- lavender.mk
+++ lavender.mk
@@ -9,10 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common RevengeOS stuff
-$(call inherit-product, vendor/revengeos/config/common.mk)
-TARGET_BOOT_ANIMATION_RES := 1080
-REVENGEOS_BUILDTYPE := OFFICIAL
+# Inherit GMS
+WITH_GMS := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -20,7 +18,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := revengeos_lavender
+PRODUCT_NAME := lavender
 PRODUCT_MODEL := Redmi Note 7
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
